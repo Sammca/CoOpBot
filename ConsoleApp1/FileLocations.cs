81067ab329a75c6957f910f2fed4d72662652a74
Richard Tranter <richard.tranter@hotmail.co.uk>
2017-06-08 15:57:06 +0100

    Xml backup and restore. File locations defined only once now in static class

---
+++ ConsoleApp1/FileLocations.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoOpBot
+{
+    public static class FileLocations
+    {
+        public static string xmlParameters()
+        {
+            return Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\CoOpBotParameters.xml";
+        }
+        public static string backupXML()
+        {
+            string fullPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\CoOpBotParameters.xml";
+            string backupPath = "";
+            string[] pathArray;
+
+            pathArray = fullPath.Split('\\');
+            pathArray = pathArray.Take(pathArray.Length - 6).ToArray();
+
+            for (int i = 0; i < pathArray.Length; i++)
+            {
+                if (i > 0)
+                {
+                    backupPath += '\\';
+                }
+                backupPath += pathArray[i];
+
+            }
+
+            backupPath += @"\CoOpBotParametersBAK.xml";
+
+            return backupPath;
+        }
+    }
+}
